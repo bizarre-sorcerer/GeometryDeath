3122e27c1770ef1b92dce45a52819cf9804b6a65
Aidar <65452919+bizarre-sorcerer@users.noreply.github.com>
2025-06-17 13:51:56 +0500

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,19 @@
 
+<?php
+$host = 'localhost';
+$db   = 'my_site';
+$user = 'root';      // замени на своего
+$pass = '';          // пароль, если есть
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     exit('Ошибка подключения к БД: ' . $e->getMessage());
+}
